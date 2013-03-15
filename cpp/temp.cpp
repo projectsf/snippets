c00ef36016d9562bc1e81601d9eee159648dad23
Scott Maroney <scott.maroney@gmail.com>
2013-03-15 14:31:54 -0700

    Clean up

--- cpp/temp.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <iostream>
-using namespace std;
-int main()
-{
-	uint8_t valueOne =  154;
-	char valueTwo = static_cast<char>(valueOne);
-	cout << "Value: " << static_cast<int>(valueTwo) << endl;
-}
